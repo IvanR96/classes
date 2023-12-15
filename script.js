a2be4442ad38a105b2989f2b0286623789326892
IvanR96 <irios96@hotmail.com>
2023-12-14 20:58:38 -0800

    class examples added

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let obj = {
 */
 
 /*
-// getter example && setter example
+// *********getter example && setter example**************
 document.addEventListener("DOMContentLoaded", function(){
 let gameInfo = {
     name: "Doom 3",
@@ -44,4 +44,63 @@ text.textContent = gameInfo.fullInfo;
 
 // *********CLASS EXAMPLE HERE BELOW**********
 
+/*
+document.addEventListener("DOMContentLoaded", function(){
+class User{
+    constructor(name){
+        this.name = name;
+    }
 
+    sayHello(){
+        alert(this.name);
+    }
+}
+
+let user = new User("Johnny Bravo");
+
+
+text = document.getElementById('info');
+
+text.textContent = user.sayHello();
+
+});
+*/
+
+
+
+// **********CLASS EXAMPLE 2 WITH SETTER/GETTER**********
+/*
+class User{
+    constructor(name){
+        // invokes the setter
+        this.name = name;
+    }
+
+    // getter 
+    get name(){
+        return this._name;
+    }
+
+    // setter
+    set name(value){
+        if (value.length < 4){
+            alert("name is too short");
+            return;
+        }
+        this._name = value;
+    }
+
+}
+
+
+let user = new User("Carmine");
+
+alert(user.name);
+
+
+user = new User(""); // error since name is too short
+
+*/
+
+
+// ************ BUTTON CLASS EXAMPLE*************
